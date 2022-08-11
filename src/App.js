92e66bbd3e30f1ded8fec402950ab6e13c6f0a64
Ellis Williams <Ellis.Williams@muddyboots.com>
2022-08-11 17:12:09 +0100

    updating our directory page with directory component and category item component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
-import { useEffect, lazy, Suspense } from 'react';
-import { useDispatch } from 'react-redux';
-
-import { Routes, Route } from 'react-router-dom';
-
-import Spinner from './components/spinner/spinner.component';
-import { checkUserSession } from './store/user/user.action';
-import { GlobalStyle } from './global.styles';
-
-const Navigation = lazy(() =>
-  import('./routes/navigation/navigation.component')
-);
-const Shop = lazy(() => import('./routes/shop/shop.component'));
-const Checkout = lazy(() => import('./routes/checkout/checkout.component'));
-const Home = lazy(() => import('./routes/home/home.component'));
-const Authentication = lazy(() =>
-  import('./routes/authentication/authentication.component')
-);
+import Directory from "./components/directory/directory.component";
 
 const App = () => {
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(checkUserSession());
-  }, []);
-
+  const categories = [
+    {
+      "id": 1,
+      "title": "hats",
+      "imageUrl": "https://i.ibb.co/cvpntL1/hats.png"
+    },
+    {
+      "id": 2,
+      "title": "jackets",
+      "imageUrl": "https://i.ibb.co/px2tCc3/jackets.png"
+    },
+    {
+      "id": 3,
+      "title": "sneakers",
+      "imageUrl": "https://i.ibb.co/0jqHpnp/sneakers.png"
+    },
+    {
+      "id": 4,
+      "title": "womens",
+      "imageUrl": "https://i.ibb.co/GCCdy8t/womens.png"
+    },
+    {
+      "id": 5,
+      "title": "mens",
+      "imageUrl": "https://i.ibb.co/R70vBrQ/men.png"
+    }
+  ];
+  
   return (
-    <Suspense fallback={<Spinner />}>
-      <GlobalStyle />
-      <Routes>
-        <Route path='/' element={<Navigation />}>
-          <Route index element={<Home />} />
-          <Route path='shop/*' element={<Shop />} />
-          <Route path='auth' element={<Authentication />} />
-          <Route path='checkout' element={<Checkout />} />
-        </Route>
-      </Routes>
-    </Suspense>
-  );
-};
+    <Directory categories={categories}></Directory>
+);
+      };
 
 export default App;
